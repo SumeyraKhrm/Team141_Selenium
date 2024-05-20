e68a0b76b388c0088e199fdd8f7968771c5f38a2
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-20 20:43:16 +0200

    version

--- src/test/java/Tests/day04_locators_maven/C03_RelativeLocators.java
+++ src/test/java/Tests/day04_locators_maven/C03_RelativeLocators.java
@@ -1,6 +1,37 @@
 package Tests.day04_locators_maven;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C03_RelativeLocators {
+    public static void main(String[] args) {
+
+           /*
+            Relative locator selenium 4 ile tanitilan yeni bir ozellik..
+            EGER
+            bir webElement'i gordugumuz ve HTML kodlarina ulastigimiz halde
+            locate'imiz calismiyor ve webElement'i kullanamiyorsak
+            ve etrafinda kullanabildigimiz baska webElementler varsa
+
+            bu webelementi ulastigimiz bir bilgisi ve
+            etrafindaki kullanabildigimiz webElementler sayesinde kullanabiliriz
+
+            WebElement abc = elementin id'si "idAbc" ve suResmin altinda
+         */
+
+
+        System.setProperty("Webdriver.chrome.driver","kurulumDosyalari/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+
+
+
+    }
 
 
 }

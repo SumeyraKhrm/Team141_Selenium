84462fc4177bfb17120fb1096f5c36335f29a131
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-05-20 21:47:35 +0200

    version

--- src/test/java/Tests/day04_locators_maven/C03_RelativeLocators.java
+++ src/test/java/Tests/day04_locators_maven/C03_RelativeLocators.java
@@ -1,12 +1,15 @@
 package Tests.day04_locators_maven;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 
 import java.time.Duration;
 
 public class C03_RelativeLocators {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
            /*
             Relative locator selenium 4 ile tanitilan yeni bir ozellik..
@@ -26,6 +29,87 @@ public class C03_RelativeLocators {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //1 ) https://testotomasyonu.com/relativeLocators adresine gidin
+        driver.get("https://testotomasyonu.com/relativeLocators");
+
+
+        //2 ) DSLR Camera’yi  etrafindaki elementleri kullanarak
+        //    3 farkli relative locator ile locate edip tiklayin
+
+        // relative locater'i kullanmak icin
+        // A- kullanmak istediginiz webElement'e ait bir locate bilgisi
+        //    <img id="pic7_thumb" src="https://testotomasyonu.com/uploads/product/1688040635pexels-pixabay-274973-removebg-preview.png" alt="">
+        //    biz id = "pic7_thumb" kullanalim
+
+
+        // B- etrafinda kullanilabilecek bir webElement'e ihtiyacimiz var
+        WebElement appleKulaklik = driver.findElement(By.xpath("//*[@id='pic6_thumb']"));
+
+        WebElement dslrCamera1 = driver.findElement (RelativeLocator
+                .with(By.id("pic7_thumb")).toRightOf(appleKulaklik));
+
+        dslrCamera1.click();
+
+        //3 ) Acilan urunun DSLR Camera oldugunu test edin.
+
+        WebElement urunIsimElementi = driver.findElement(By.xpath("//div[@class=' heading-sm mb-4']"));
+
+        String expectedIsim ="DSLR Camera";
+        String actualisim = urunIsimElementi.getText();
+
+        if(expectedIsim.equals(actualisim)){
+            System.out.println("DSLR camera testi Passed");
+        }else
+            System.out.println("DSLR camera testi Failed");
+
+
+        // ============================
+        // geriye donup baska bir komsusuyla bir kere daha locate edelim
+        // BatKulakligin altinda diyelim
+
+
+        driver.get("https://testotomasyonu.com/relativeLocators");
+
+        // relative locator icin 2 bilgiye ihtiyac var
+
+        // A- kullanmak istedigimiz webElementin locate blgisine
+        //    biz tagName = 'img' kullanalim
+
+        // B- etrafinda kullanabilecegimiz bir WebElement
+
+        WebElement batKulaklik = driver.findElement(By.id("pic2_thumb"));
+
+        WebElement dslrCamera2 = driver.findElement(
+                RelativeLocator.with(By.tagName("img"))
+                        .below(batKulaklik));
+
+        dslrCamera2.click();
+
+        /*
+            StaleElementReferenceException
+
+            daha onceden locate edip kullandigimiz bir WebElementi
+            baska sayfalara gittikten sonra yeniden kullanmak istedigimizde olusur
+
+            Selenium bize "tamam o elementi locate etmistin ama
+            o islemden sonra coook sey degisti, senin locate bayatladi" der
+         */
+        urunIsimElementi = driver.findElement(By.xpath("//div[@class=' heading-sm mb-4']"));
+
+        expectedIsim = "DSLR Camera";
+        actualisim = urunIsimElementi.getText();
+
+        if (expectedIsim.equals(actualisim)){
+            System.out.println("DSLR camera testi PASSED");
+        }else  System.out.println("DSLR camera testi FAILED");
+
+
+
+        Thread.sleep(3000);
+        driver.quit();
+
+
 
 
 
